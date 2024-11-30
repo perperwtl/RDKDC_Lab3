5e09fc3631f8d7480f08fe5a07dbc8d8ec0f2c36
perperwtl <134026174+perperwtl@users.noreply.github.com>
2024-11-30 01:50:14 -0500

    update test for 1,2,3

--- lab3.m
+++ lab3.m
@@ -4,9 +4,9 @@ clear all
 ur5 = ur5_interface();
 tf_frame.get_tf_tree();
 
-is_test_ur5FwdKin = 1;
+is_test_ur5FwdKin = 0;
 is_test_ur5BodyJacobian = 0;
-is_test_manipulability = 0;
+is_test_manipulability = 1;
 is_test_getXi = 0;
 is_test_ur5RRcontrol = 0;
 
@@ -18,13 +18,13 @@ if is_test_ur5FwdKin
 
     % home pose
     q = [0;
-        -pi / 2;
+        -pi/2;
         0;
         -pi/2;
         0;
         0];
 
-    angle = 10 * pi / 180;
+    angle = 0;
 
     q = q + angle;
 
@@ -49,6 +49,55 @@ end
 
 %% Test ur5BodyJacobian.m
 if is_test_ur5BodyJacobian
+    % Parameters
+    epsilon = 1e-6;           % Small perturbation value
+    q = rand(6, 1) * 2 * pi;  % Random joint configuration in [0, 2π]
+    
+    % Compute the exact Jacobian using ur5BodyJacobian
+    gst = ur5FwdKin(q);       % Forward kinematics to get gst
+    J = ur5BodyJacobian(q);  % Exact Jacobian
+    
+    % Initialize approximate Jacobian
+    J_approx = zeros(6, 6);
+    
+    % Compute approximate Jacobian column-by-column
+    for i = 1:6
+        % Perturb the i-th joint angle by +epsilon and -epsilon
+        q_plus = q;
+        q_minus = q;
+        q_plus(i) = q_plus(i) + epsilon;
+        q_minus(i) = q_minus(i) - epsilon;
+    
+        % Forward kinematics for perturbed configurations
+        gst_plus = ur5FwdKin(q_plus);
+        gst_minus = ur5FwdKin(q_minus);
+    
+        % Central difference approximation for ∂g/∂q_i
+        dg_dqi = (gst_plus - gst_minus) / (2 * epsilon);
+    
+        % Compute g⁻¹ ∂g/∂q_i
+        twist_matrix = inv(gst) * dg_dqi;
+    
+        % Twistify: Convert the 4x4 matrix to a 6D twist vector
+        xi = Twistify(twist_matrix);
+    
+        % Store the twist as the i-th column of J_approx
+        J_approx(:, i) = xi;
+    end
+    
+    % Compute the error norm ||J_approx - J_actual||
+    errorNorm = norm(J_approx - J);
+    
+    % Display results
+    disp('Actual Jacobian (J):');
+    disp(J);
+    
+    disp('Approximate Jacobian (J_approx):');
+    disp(J_approx);
+    
+    disp('Norm of the error ||J_approx - J_actual||:');
+    disp(errorNorm);
+    
 end
 
 %% Test manipulability.m
@@ -58,11 +107,11 @@ if is_test_manipulability
     num_points = length(theta3_values);
     
     % Random pose for the other joint angles (avoid singularities)
-    theta1 = rand() * 2 * pi; % Random angle in [0, 2π]
-    theta2 = rand() * 2 * pi;
-    theta4 = rand() * 2 * pi;
-    theta5 = rand() * 2 * pi;
-    theta6 = rand() * 2 * pi;
+    theta1 = 1.5 * pi; % Random angle in [0, 2π]
+    theta2 = 1.5 * pi;
+    theta4 = 1.5 * pi;
+    theta5 = 1.5 * pi;
+    theta6 = 1.5 * pi;
     
     % Initialize arrays to store manipulability measures
     sigmamin_values = zeros(1, num_points);
@@ -140,6 +189,37 @@ if is_test_getXi
 
 end
 
+
+% Test ur5RRcontrol.m
+if is_test_ur5RRcontrol
+end
+
+
+%% Helper Functions
+
+function xi = Twistify(g)
+    % Convert a 4x4 matrix to a 6x1 twist vector
+    %
+    % Input:
+    %   g - 4x4 homogeneous transformation matrix
+    % Output:
+    %   xi - 6x1 twist vector [v; omega]
+
+    % Extract rotation (upper-left 3x3 block)
+    R = g(1:3, 1:3);
+
+    % Force skew-symmetric adjustment
+    R_skew = (R - R') / 2;
+
+    omega = SkewToAxis(R_skew);
+    v = g(1:3, 4);
+    xi = [v; omega];
+end
+
+function w = SkewToAxis(S)
+    w = [S(3, 2); S(1, 3); S(2, 1)];
+end
+
 %% Test the getXi function by reconstructing and comparing matrices
 function testgetXi(g)    
     xi = getXi(g);
@@ -179,6 +259,3 @@ function M_h = homogeneous_matrix(angles_XYZ,translation)
 end
 
 
-% Test ur5RRcontrol.m
-if is_test_ur5RRcontrol
-end
